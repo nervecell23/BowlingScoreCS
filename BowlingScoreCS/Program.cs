0d16e1139ff549912fa5a64ccb0d075ec6c06056
Chuan Cheng <c_chuan23@hotmail.com>
2019-11-04 16:11:32 +0000

    First commit

---
+++ BowlingScoreCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BowlingScoreCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
